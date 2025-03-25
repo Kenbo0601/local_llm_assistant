e4d1c96fc6acf1727b74efbc3af6c8a20686a705
kenbo0601 <eug.ken0601@gmail.com>
2025-03-24 23:57:38 -0700

    added doc_loader class

--- scripts/loader.py
+++ scripts/loader.py
@@ -7,17 +7,7 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores import Chroma
 
 
-loader = PyMuPDFLoader("../data/paper1.pdf")
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-chunks = text_splitter.split_documents(docs)
-
-for i, chunk in enumerate(chunks):
-    print(f"\n--- Chunk {i+1} ---")
-    print(chunk.page_content)
-
-embedding = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
+#embedding = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
 
 # testing db connection
 '''
@@ -28,6 +18,22 @@ cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cursor.fetchall()
 print("Tables:", tables)'''
 
-
+# Class for processing documents - TODO: we need to embed docs into chroma database 
 class DocumentLoader:
-    pass
+    def __init__(self, path="../data"):
+        self.dir_path = path # path to the data directory
+
+    # load and process one pdf file TODO: need to add something more, either return the chunks or call chroma_db manager 
+    def load_pdf(self, doc_name):
+        loader = PyMuPDFLoader("../data/" + doc_name)
+        docs = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        chunks = text_splitter.split_documents(docs)
+
+        for i, chunk in enumerate(chunks):
+            print(f"\n--- Chunk {i+1} ---")
+            print(chunk.page_content)
+
+
+doc_loader = DocumentLoader()
+doc_loader.load_pdf("paper1.pdf")
