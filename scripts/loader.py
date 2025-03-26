c14e211ba35f1f53138c1c0bdf3d943489e78e33
kenbo0601 <eug.ken0601@gmail.com>
2025-03-25 22:36:08 -0700

    added function for extracting .db file

--- scripts/loader.py
+++ scripts/loader.py
@@ -18,8 +18,35 @@ print("Tables:", tables)'''
 
 # Class for processing documents - TODO: we need to embed docs into chroma database 
 class DocumentLoader:
+
     def __init__(self, path="../data/"):
         self.dir_path = path # path to the data directory
+
+    #  extract db file - get table names and attributes 
+    def load_schema(self, db_name):
+        db_path = self.dir_path + "db/" + db_name
+        try:
+            conn = sqlite3.connect(db_path)
+            print("Connection successful.")
+        except FileNotFoundError:
+            print("Error: File not found.")
+
+        cursor = conn.cursor() 
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = [t[0] for t in cursor.fetchall()]
+
+        schema_docs = []
+
+        # loop through each table to get attributes
+        for table in tables:
+            cursor.execute(f"PRAGMA table_info({table})")
+            columns = [row[1] for row in cursor.fetchall()]
+            schema_text = f"Table: {table}\nColumns: {', '.join(columns)}"
+            print(schema_text)
+            schema_docs.append(schema_text) # save them into the list
+
+        return schema_docs
+
 
     # load and process one pdf file TODO: need to add something more, either return the chunks or call chroma_db manager 
     def load_pdf(self, doc_name):
@@ -40,5 +67,3 @@ class DocumentLoader:
         all_chunks = [] # store chunks of all pdfs 
         return all_chunks
 
-doc_loader = DocumentLoader()
-doc_loader.load_pdf("paper1.pdf")
--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,9 @@
 from loader import DocumentLoader
 from chroma_db_manager import ChromaDBManager
 
+doc_loader = DocumentLoader()
+doc_loader.load_schema("sqlite-sakila.db")
+
 '''
 doc_loader = DocumentLoader()
 chunks = doc_loader.load_pdf("paper1.pdf")
@@ -13,6 +16,6 @@ chroma.create_collection("pdf_chunks")
 chroma.add_documents_to_collection("pdf_chunks", [chunks[0]])'''
 
 # testing - above code added pdf chunks into chroma database, so after running the code above, checking if it exists in db and pull data
-chroma = ChromaDBManager()
-c = chroma.get_collection("pdf_chunks")
-print(c)
+#chroma = ChromaDBManager()
+#c = chroma.get_collection("pdf_chunks")
+#print(c)
