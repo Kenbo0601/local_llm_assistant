a0d10ee91ae5e51ad0dfcd4076ca76e6d5dcfe25
kenbo0601 <eug.ken0601@gmail.com>
2025-03-26 12:57:25 -0700

    removed pdf function

--- scripts/managers/chroma_db_manager.py
+++ scripts/managers/chroma_db_manager.py
@@ -2,13 +2,12 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from pathlib import Path
 
-# TODO: we probably should have one collection for one .db file instead of storing whole .db files in one collection 
 
 # Base Manager Class
 class ChromaDBManager:
     def __init__(self):
         self.base_path = Path(__file__).resolve().parents[2] # get absolute path to project root (2 levels up from this file)
-        self.db_path = str(self.base_path / "chroma_db") # chroma_db folder should be located in the top level - we also need to convert Path object -> str
+        self.db_path = str(self.base_path / "chroma_db") # chroma_db folder should be located in the top level - we also need to convert Path object -> str for later use
         self.embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2") # sentence transformer model - this model is small and fast
         self.collections = {}
 
@@ -38,17 +37,3 @@ class ChromaDBManager:
             print(f"[+] Loaded collection '{collection_name}' from disk.")
             return db
 
-
-
-# Child Class - Handle PDF documents
-class PDFManager(ChromaDBManager):
-
-    def __init__(self):
-        super().__init__()
-    
-    # add documents into existing collection - need to specify collection name 
-    def add_documents_to_collection(self, collection_name, documents):
-        if collection_name not in self.collections:
-            raise ValueError(f"Collection '{collection_name}' not found. Create it first.")
-        self.collections[collection_name].add_documents(documents)
-        print(f"[+] Added {len(documents)} chunks to collection '{collection_name}'.")
